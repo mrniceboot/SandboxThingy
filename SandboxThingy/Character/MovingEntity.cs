8e94c1d27615d1c932cc15373eb3f71424f36cc4
Tobiky <andreas.hammarstrand@gmail.com>
2017-11-05 23:37:10 +0100

    A lot of basic stuff, not done
    Incomplete but I reckon it's about 50% complete as a branch.

---
+++ SandboxThingy/Character/MovingEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SandboxThingy.Character
+{
+    public abstract class MovingEntity : Entity, IMoveable
+    {
+        public int Speed { get; set; }
+
+        public void MoveXMinus() => location.X -= Speed;
+        public void MoveXPlus() => location.X +=  Speed;
+        public void MoveYMinus() => location.Y -= Speed;
+        public void MoveYPlus() => location.Y +=  Speed;
+
+        public MovingEntity(Point location, int speed, Texture2D texture, uint id) : base(location, texture, id = 0) => Speed = speed;
+        public MovingEntity(int x, int y, int speed, Texture2D texture, uint id) : base(x, y, texture, id = 0) => Speed = speed;
+    }
+}
